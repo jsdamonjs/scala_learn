9aca9df8dea42ec7da54a0caa170fa756bd41f7d
jsdamonjs <damonjs@163.com>
2018-05-06 19:22:08 +0800

    set log level

--- scala/cn/damon/tasks/test.scala
+++ scala/cn/damon/tasks/test.scala
@@ -5,8 +5,9 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.log4j.{Level, Logger}
 object test {
-  val sparksession = SparkSession.builder().appName("test").getOrCreate()
+  val sparksession = SparkSession.builder().appName("test").master("local[1]").getOrCreate()
   Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+  Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
   def main(args: Array[String]): Unit = {
     val util = new my_util(sparksession)
     val schema_1 = StructType(
